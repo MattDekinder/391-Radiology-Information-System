adfa92158756f58eca11676434259b46af6a53e5
Evan Sinleton <esinglet@ualberta.ca>
2014-03-15 21:30:27 -0600

    Added stuff

---
+++ change.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Change Password</title>
+</head>
+<body>
+	<form name="changep" method="post" action="change.php">
+		Old Password : <input type="text" name="username"/> <br>
+		New Password : <Input type="text" name="opassword"/> <br>
+		Confirm New Password : <input type="text" name="c_opassword"> <br>
+		<input type="submit" name="change_go" value="Change Password" >
+	</form>
+</body>
+</html>
