385ee9815793d285821697261369372c44bb936b
MattDekinder <dekinder@ualberta.ca>
2014-03-19 21:30:38 -0600

    minor changes

--- search.php
+++ search.php
@@ -22,9 +22,7 @@ if(isset($_SESSION['USER_NAME'])){
 			<h1 class="header">Radiology Search</h1>
 			<form name="user_login" method="post" action="search.php">
 				Search : <input type="text" name="search"> <br> 
-<!-- insert some kind of date picker here-->
 				<input type="submit" name="search" value="Search">
-				<input type="submit" name="change" value="Change Password">
 			</form>
 			<?php if($search){ ?>
 			<h2>Search Failed!(what does that even mean!! hoq does a search fail.)</h2>
