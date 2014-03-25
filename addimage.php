0389631b9c4b8d609e3761f7f217c2b0c5250f3e
Evan Sinleton <esinglet@ualberta.ca>
2014-03-25 14:15:55 -0600

    Branch

--- addimage.php
+++ addimage.php
@@ -1,5 +1,7 @@
 <?php
-include("database.php");
+
+//Comment back in to reenable uploading of records
+/*include("database.php");
 
 session_start();
 
@@ -9,10 +11,14 @@ if(isset($_POST['create'])){
 
 	make_record($retval, $_POST["patient"], $_POST["doctor"], $_POST["rad"], $_POST["test_type"], $_POST["pers_date"], $_POST["test_date"], $_POST["diag"], $_POST["desc"]);
 
-}
+}*/
+
+
 ?>
 <html>
 <body>
-<?php echo "Retval: ".$retval; ?>
+<form name="addimage" method="post">
+	<input type="file" name="file">
+</form>
 </body>
 </html>
