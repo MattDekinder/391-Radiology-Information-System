01249afc7039190b9f5921a7608bafd02fde3d72
Evan Singleton <esinglet@ualberta.ca>
2014-03-26 06:08:30 -0600

    added

--- data.php
+++ data.php
@@ -9,20 +9,42 @@ include('database.php');
 if(isset($_POST['sub'])){
 
 	
-	if(!empty($_POST['date'])){
+	if(($isdate = !empty($_POST['date']) ) ){
+
+		echo "date";
 
 		$d = $_POST['date'];
 
 		$duedt = explode("-", $d);
 		$date  = mktime(0, 0, 0, $duedt[1], $duedt[2], $duedt[0]);
 		$dayy  = (int)date('z', $date);
+	//The search year
 		$year = (int)date('Y', $date);
 
+	//The day in letters
+		$day_nice = (int)date('D', $date);
+	//The day in numbers
+		$day_nice_num = (int)date('d', $date);
+
+	//Week number
+		$week_num = (int)date('W', $date);
+
+	//Sets to nerest lower sunday
 		$dayw = $dayy - (int)date('w', $date);
 
 		$sunday1 = date_create_from_format('z Y', strval($dayw).' '.strval($year));
-		$day = date_create_from_format('z Y', strval($dayw).' '.strval($year));
+		echo $dayw;
+		if($dayw >= 0){
+			$sunday1 = date_create_from_format('z Y', strval($dayw).' '.strval($year));
+			$day = date_create_from_format('z Y', strval($dayw).' '.strval($year));
+		} else{
+			$dayw = 365+$dayw;
+			$sunday1 = date_create_from_format('z Y', strval($dayw).' '.strval($year));
+			$day = date_create_from_format('z Y', strval($dayw).' '.strval($year-1));
+		}
+
 		$sunday2 = $day->add(date_interval_create_from_date_string('7 days'));
+
 
 
 	//First and last day of week inclusive
@@ -49,12 +71,49 @@ if(isset($_POST['sub'])){
 		$yeard1 = $dayy1->format('Y-m-d');
 		$yeardL = $dayyL->format('Y-m-d');
 
+		$period = $_POST['period'];
 
 	}
-	$period = $_POST['period'];
-	echo $period;
-	echo "Pat:".$_POST['patient'];
+	
+	if( ($istest = !empty($_POST['testt']) ) ){
+		echo "test";
+	}
 
+	if( ($ispat = !empty($_POST['patient'])) ){
+		echo "patient";
+	}
+
+	$sql = "select count(*) from radiology_record ";
+
+	if($isdate || $istest || $ispat){
+		$sql.='where ';
+	}
+
+	if($isdate){
+		switch ((int)$period){
+			case 0: //week
+			$sql.='';
+			echo "The ".$week_num."th week of ".$year.".";
+			break;
+			case 1: //month
+			$sql.='';
+			echo "month";
+			break;
+			case 2: //year
+			$sql.='';
+			echo "year";
+			break;
+
+		}
+	}
+
+	if($istest){
+		$sql.='';
+	}
+
+	if($ispat){
+		$sql.='';
+	}
 }
 ?>
 <html>
@@ -67,14 +126,15 @@ if(isset($_POST['sub'])){
 				<label>Date: </label>
 				<input type="date" name="date"> 
 				<select name="period">
-					<option value="week">Week</option>
-					<option value="month">Month</option>
-					<option value="year">Year</option>	
+					<option value="0">Week</option>
+					<option value="1">Month</option>
+					<option value="2">Year</option>	
 				</select><br>
 				<label>Test Type: </label>
 				<select name="testt">
+					<option value=""></option>
 					<?php
-					$types = get_test_types();
+					//$types = get_test_types();
 					foreach ($types as $key => $value) {
 						echo '<option value="'.$value.'">'.$value.'</option>';
 					}
@@ -82,8 +142,9 @@ if(isset($_POST['sub'])){
 				</select> <br>
 				<label>Patient: </label>
 				<select name="patient">
+					<option value=""></option>
 					<?php
-					$patients = get_patients();
+					//$patients = get_patients();
 					foreach ($patients as $value) {
 						echo '<option value="'.$value['PERSON_ID'].'">'.$value['LAST_NAME'].', '.$value['FIRST_NAME'].'</option>';
 					}
