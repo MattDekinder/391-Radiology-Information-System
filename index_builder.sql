bc570b200df66ad5e91ba6cf4e102a37d9ab6615
MattDekinder <dekinder@ualberta.ca>
2014-03-25 20:43:36 -0600

    added drop statements for indicies

--- index_builder.sql
+++ index_builder.sql
@@ -1,3 +1,8 @@
+drop index DESCRIPTION_INDEX;
+drop index DIAGNOSIS_INDEX;
+drop index FIRSTNAME_INDEX;
+drop index LASTNAME_INDEX;
+
 create index DESCRIPTION_INDEX on RADIOLOGY_RECORD(description) indextype is ctxsys.context;
 create index DIAGNOSIS_INDEX on RADIOLOGY_RECORD(diagnosis) indextype is ctxsys.context;
 create index FIRSTNAME_INDEX on PERSONS(first_name) indextype is ctxsys.context;
