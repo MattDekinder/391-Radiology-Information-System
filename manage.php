bf84ee545e59e2788e05b20ecc3ade01f280e89c
Evan Sinleton <esinglet@ualberta.ca>
2014-03-26 22:32:10 -0600

    Adding usermanagement basics

--- manage.php
+++ manage.php
@@ -1,5 +1,13 @@
 <html>
 <body>
-	
+	<form action="update_users.php" method="post">
+		
+	</form>
+	<form action="update_persons.php" method="post">
+		
+	</form>
+	<form action="update_fdoctor.php" method="post">
+		
+	</form>
 </body>
 </html>
